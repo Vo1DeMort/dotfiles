55f74d93a834c3fda28097e17a311e0653a8865b
Ye Yint Aung <mryeyintaung669@gmail.com>
2025-02-09 21:18:08 +0630

    mod snaks picker of nvim

--- .config/nvim/lua/plugins/neo-tree.lua
+++
@@ -1,7 +0,0 @@
--- move the neo-tree to the right
-return {
-  "nvim-neo-tree/neo-tree.nvim",
-  opts = {
-    window = { position = "right" },
-  },
-}
---
+++ .config/nvim/lua/plugins/snak-picker.lua
@@ -0,0 +1,11 @@
+-- move the neo-tree to the right
+return {
+	{
+		"folke/snacks.nvim",
+		opts = {
+			picker = {
+				layout = { layout = { position = "right" } },
+			},
+		},
+	},
+}
