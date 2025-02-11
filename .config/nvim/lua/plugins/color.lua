e5a2413c36d07df65d464933912f828cbda8f91b
Ye Yint Aung <mryeyintaung669@gmail.com>
2025-02-11 21:02:55 +0630

    neo-solarized

--- .config/nvim/lua/plugins/color.lua
+++ .config/nvim/lua/plugins/color.lua
@@ -1,20 +1,27 @@
 return {
+
   {
-    "sainnhe/gruvbox-material",
-    lazy = false,
-    priority = 1000,
+    "Tsuzat/NeoSolarized.nvim",
+    lazy = false, -- make sure we load this during startup if it is your main colorscheme
+    priority = 1000, -- make sure to load this before all the other start plugins
     config = function()
-      vim.g.gruvbox_material_enable_italic = false
-      vim.g.gruvbox_material_background = "hard"
-      vim.g.gruvbox_material_better_performance = 1
-      vim.g.gruvbox_material_foreground = "original"
+      require("NeoSolarized").setup({
+        transparent = false,
+        enable_italics = false,
+        on_highlights = function(highlights, colors)
+          highlights.SignColumn = { bg = "none" } -- Removes the SignColumn background
+          highlights.LineNr = { bg = "none" } -- Removes the LineNr background
+          highlights.CursorLineNr = { bg = "none", fg = colors.orange } -- Optional: Highlight current line number
+        end,
+      })
     end,
   },
 
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "gruvbox-material",
+      --colorscheme = "gruvbox-material",
+      colorscheme = "NeoSolarized",
     },
   },
 }
