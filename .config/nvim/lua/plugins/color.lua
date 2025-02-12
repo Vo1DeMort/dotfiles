744aff7789c1e004f78f9aa6cfe4e9cd505dd4bc
Ye Yint Aung <mryeyintaung669@gmail.com>
2025-02-12 13:04:43 +0630

    osaka solarized

--- .config/nvim/lua/plugins/color.lua
+++ .config/nvim/lua/plugins/color.lua
@@ -1,27 +1,18 @@
 return {
 
   {
-    "Tsuzat/NeoSolarized.nvim",
-    lazy = false, -- make sure we load this during startup if it is your main colorscheme
-    priority = 1000, -- make sure to load this before all the other start plugins
-    config = function()
-      require("NeoSolarized").setup({
-        transparent = false,
-        enable_italics = false,
-        on_highlights = function(highlights, colors)
-          highlights.SignColumn = { bg = "none" } -- Removes the SignColumn background
-          highlights.LineNr = { bg = "none" } -- Removes the LineNr background
-          highlights.CursorLineNr = { bg = "none", fg = colors.orange } -- Optional: Highlight current line number
-        end,
-      })
-    end,
+    "craftzdog/solarized-osaka.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = {
+      transparent = false,
+    },
   },
 
   {
     "LazyVim/LazyVim",
     opts = {
-      --colorscheme = "gruvbox-material",
-      colorscheme = "NeoSolarized",
+      colorscheme = "solarized-osaka",
     },
   },
 }
