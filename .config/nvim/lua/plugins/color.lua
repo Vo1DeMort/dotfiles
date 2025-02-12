efe246215de149876bd530320b89171f22d82f06
Ye Yint Aung <mryeyintaung669@gmail.com>
2025-02-12 23:16:39 +0630

    moving to opensuse

--- .config/nvim/lua/plugins/color.lua
+++ .config/nvim/lua/plugins/color.lua
@@ -8,11 +8,27 @@ return {
       transparent = false,
     },
   },
+  {
+    "maxmx03/solarized.nvim",
+    lazy = false,
+    priority = 1000,
+    ---@type solarized.config
+    opts = {
+      palette = "solarized", -- solarized (default) | selenized
+      variant = "autumn", -- "spring" | "summer" | "autumn" | "winter" (default)
+    },
+    config = function(_, opts)
+      vim.o.termguicolors = true
+      vim.o.background = "dark"
+      require("solarized").setup(opts)
+    end,
+  },
 
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "solarized-osaka",
+      --colorscheme = "solarized-osaka",
+      colorscheme = "solarized",
     },
   },
 }
