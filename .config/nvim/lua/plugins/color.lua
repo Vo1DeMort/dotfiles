11fac5e079f3ee936274be3a2e1028c5d11d20e4
Ye Yint <mryeyintaung669@gmail.com>
2025-03-13 23:25:46 +0630

    add qtile config

--- .config/nvim/lua/plugins/color.lua
+++ .config/nvim/lua/plugins/color.lua
@@ -14,6 +14,22 @@ return {
       })
     end,
   },
+  {
+    "rebelot/kanagawa.nvim",
+    config = function()
+      require("kanagawa").setup({
+        colors = {
+          theme = {
+            all = {
+              ui = {
+                bg_gutter = "none",
+              },
+            },
+          },
+        },
+      })
+    end,
+  },
 
   {
     "LazyVim/LazyVim",
