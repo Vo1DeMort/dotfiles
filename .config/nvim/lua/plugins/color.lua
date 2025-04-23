66e063f093c90d696b9de5520d29358f85b09913
Ye Yint <mryeyintaung669@gmail.com>
2025-04-24 00:29:46 +0630

    nightfox as main theme

--- .config/nvim/lua/plugins/color.lua
+++ .config/nvim/lua/plugins/color.lua
@@ -1,18 +1,6 @@
 return {
 
   {
-    "sainnhe/sonokai",
-    lazy = false,
-    priority = 1000,
-    config = function()
-      -- Optionally configure and load the colorscheme
-      -- directly inside the plugin declaration.
-      -- maia
-      --vim.g.sonokai_style = "maia"
-      vim.g.sonokai_better_performance = 1
-    end,
-  },
-  {
     "maxmx03/solarized.nvim",
     opts = {
       variant = "autumn", -- "spring" | "summer" | "autumn" | "winter" (default)
@@ -26,24 +14,13 @@ return {
       },
     },
   },
-  {
-    "catppuccin/nvim",
-    name = "catppuccin",
-    priority = 1000,
-    config = function()
-      require("catppuccin").setup({
-        --flavour = "mocha",
-        transparent_background = true,
-      })
-    end,
-  },
+  { "EdenEast/nightfox.nvim" },
 
   {
     "LazyVim/LazyVim",
     opts = {
-      --colorscheme = "sonokai",
       --colorscheme = "solarized",
-      colorscheme = "catppuccin",
+      colorscheme = "nightfox",
     },
   },
 }
