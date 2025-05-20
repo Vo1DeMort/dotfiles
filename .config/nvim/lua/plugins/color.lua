988936e2e24c06494194ca4444487f68a80d2028
Ye Yint <mryeyintaung669@gmail.com>
2025-05-20 21:25:07 +0630

    solarized

--- .config/nvim/lua/plugins/color.lua
+++ .config/nvim/lua/plugins/color.lua
@@ -13,11 +13,28 @@ return {
             })
         end,
     },
+    {
+        "maxmx03/solarized.nvim",
+        lazy = false,
+        priority = 1000,
+        ---@type solarized.config
+        opts = {
+            variant = "autumn", -- "spring" | "summer" | "autumn" | "winter" (default)
+            transparent = {
+                enabled = true, -- Master switch to enable transparency
+            },
+        },
+        config = function(_, opts)
+            vim.o.termguicolors = true
+            vim.o.background = "dark"
+            require("solarized").setup(opts)
+        end,
+    },
 
     {
         "LazyVim/LazyVim",
         opts = {
-            colorscheme = "onedark",
+            colorscheme = "solarized",
         },
     },
 }
