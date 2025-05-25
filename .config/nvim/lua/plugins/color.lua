074816c73cbaf34b719b5d749442f6ca063d5a21
Ye Yint <mryeyintaung669@gmail.com>
2025-05-25 20:52:46 +0630

    gruvbox material

--- .config/nvim/lua/plugins/color.lua
+++ .config/nvim/lua/plugins/color.lua
@@ -6,35 +6,57 @@ return {
         config = function()
             require("onedark").setup({
                 style = "deep",
-                transparent = true,
+                transparent = false,
                 lualine = {
-                    transparent = true,
+                    transparent = false,
                 },
             })
         end,
     },
     {
-        "maxmx03/solarized.nvim",
+        "rebelot/kanagawa.nvim",
+        config = function()
+            require("kanagawa").setup({
+                colors = {
+                    theme = {
+                        all = {
+                            ui = {
+                                bg_gutter = "none",
+                            },
+                        },
+                    },
+                },
+            })
+        end,
+    },
+    {
+        "rose-pine/neovim",
+        name = "rose-pine",
+        config = function()
+            require("rose-pine").setup({
+                styles = {
+                    bold = true,
+                    italic = true,
+                    transparency = false,
+                },
+            })
+        end,
+    },
+    {
+        "sainnhe/gruvbox-material",
         lazy = false,
         priority = 1000,
-        ---@type solarized.config
-        opts = {
-            variant = "autumn", -- "spring" | "summer" | "autumn" | "winter" (default)
-            transparent = {
-                enabled = true, -- Master switch to enable transparency
-            },
-        },
-        config = function(_, opts)
-            vim.o.termguicolors = true
-            vim.o.background = "dark"
-            require("solarized").setup(opts)
+        config = function()
+            vim.g.gruvbox_material_enable_italic = true
+            vim.g.gruvbox_material_background = "medium"
+            vim.g.gruvbox_material_foreground = "original"
         end,
     },
 
     {
         "LazyVim/LazyVim",
         opts = {
-            colorscheme = "solarized",
+            colorscheme = "gruvbox-material",
         },
     },
 }
