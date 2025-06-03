9b93548d824ddcd9a12c391b072926e78b62fba8
Ye Yint <mryeyintaung669@gmail.com>
2025-06-03 22:10:24 +0630

    only gruvbox

--- .config/nvim/lua/plugins/color.lua
+++ .config/nvim/lua/plugins/color.lua
@@ -1,48 +1,6 @@
 return {
 
     {
-        "navarasu/onedark.nvim",
-        priority = 1000,
-        config = function()
-            require("onedark").setup({
-                style = "deep",
-                transparent = false,
-                lualine = {
-                    transparent = false,
-                },
-            })
-        end,
-    },
-    {
-        "rebelot/kanagawa.nvim",
-        config = function()
-            require("kanagawa").setup({
-                colors = {
-                    theme = {
-                        all = {
-                            ui = {
-                                bg_gutter = "none",
-                            },
-                        },
-                    },
-                },
-            })
-        end,
-    },
-    {
-        "rose-pine/neovim",
-        name = "rose-pine",
-        config = function()
-            require("rose-pine").setup({
-                styles = {
-                    bold = true,
-                    italic = true,
-                    transparency = true,
-                },
-            })
-        end,
-    },
-    {
         "sainnhe/gruvbox-material",
         lazy = false,
         priority = 1000,
