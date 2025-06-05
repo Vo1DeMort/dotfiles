8bdc397b918b2483d19ba320cc38c96b0a423a59
Ye Yint <mryeyintaung669@gmail.com>
2025-06-06 05:33:48 +0630

    amor fati

--- .config/nvim/lua/plugins/color.lua
+++ .config/nvim/lua/plugins/color.lua
@@ -10,11 +10,27 @@ return {
             vim.g.gruvbox_material_foreground = "original"
         end,
     },
+    {
+        "neanias/everforest-nvim",
+        version = false,
+        lazy = false,
+        priority = 1000,
+    },
+    {
+        "sainnhe/sonokai",
+        lazy = false,
+        priority = 1000,
+        config = function()
+            -- Optionally configure and load the colorscheme
+            -- directly inside the plugin declaration.
+            vim.g.sonokai_enable_italic = true
+        end,
+    },
 
     {
         "LazyVim/LazyVim",
         opts = {
-            colorscheme = "gruvbox-material",
+            colorscheme = "sonokai",
         },
     },
 }
