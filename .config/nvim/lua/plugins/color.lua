eb6a7c2d14d321a91e1415c71053bcd99385282e
Ye Yint <mryeyintaung669@gmail.com>
2025-06-08 17:57:49 +0630

    monokai machine

--- .config/nvim/lua/plugins/color.lua
+++ .config/nvim/lua/plugins/color.lua
@@ -8,29 +8,16 @@ return {
             vim.g.gruvbox_material_enable_italic = true
             vim.g.gruvbox_material_background = "medium"
             vim.g.gruvbox_material_foreground = "original"
+            vim.g.gruvbox_material_transparent_background = "0"
         end,
     },
 
-    {
-        "maxmx03/solarized.nvim",
-        lazy = false,
-        priority = 1000,
-        opts = {
-            variant = "autumn", -- "spring" | "summer" | "autumn" | "winter" (default)
-            transparent = {
-                enabled = true, -- Master switch to enable transparency
-            },
-        },
-        config = function(_, opts)
-            vim.o.termguicolors = true
-            vim.o.background = "dark"
-            require("solarized").setup(opts)
-        end,
-    },
+    { "loctvl842/monokai-pro.nvim" },
+
     {
         "LazyVim/LazyVim",
         opts = {
-            colorscheme = "solarized",
+            colorscheme = "monokai-pro-machine",
         },
     },
 }
