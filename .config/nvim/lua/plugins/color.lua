ec4d0a07fcf5f91d7553af7d9d53d0e0bd79d3b4
Ye Yint <mryeyintaung669@gmail.com>
2025-07-07 20:29:00 +0630

    waybar config

--- .config/nvim/lua/plugins/color.lua
+++ .config/nvim/lua/plugins/color.lua
@@ -12,6 +12,16 @@ return {
         end,
     },
     {
+        "navarasu/onedark.nvim",
+        priority = 1000, -- make sure to load this before all the other start plugins
+        config = function()
+            require("onedark").setup({
+                style = "darker", -- darker, deep
+            })
+        end,
+    },
+
+    {
         "LazyVim/LazyVim",
         opts = {
             colorscheme = "gruvbox-material",
