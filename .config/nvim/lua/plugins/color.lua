214ed73f55f9fd9857c14e2994ffe1793d913541
Ye Yint <mryeyintaung669@gmail.com>
2025-08-11 18:48:14 +0630

    removed nvchad

--- .config/nvim/lua/plugins/color.lua
+++ .config/nvim/lua/plugins/color.lua
@@ -1,32 +1,21 @@
 return {
 
     {
-        "folke/tokyonight.nvim",
-        lazy = false,
-        priority = 1000,
-        config = function()
-            require("tokyonight").setup({
-                style = "night",
-                styles = {
-                    keywords = { italic = false },
-                    sidebar = "normal",
-                    floats = "normal",
-                },
-            })
-        end,
-    },
-    {
         "olimorris/onedarkpro.nvim",
-        priority = 1000, -- Ensure it loads first
+        priority = 1000, 
+    config = function()
+        require("onedarkpro").setup({
+  options = {
+    transparency = true
+  }
+})
+    end
     },
-    { "xStormyy/bearded-theme.nvim" },
 
     {
         "LazyVim/LazyVim",
         opts = {
-            colorscheme = "tokyonight",
-            --colorscheme = "vaporwave",
+            colorscheme = "vaporwave",
         },
     },
-    -- https://chatgpt.com/share/688b8723-30d8-8006-90cb-ed4edf121ab3
 }
