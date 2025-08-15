031117c99bc4aee2f689cdda2d9cd42a26758a8a
Ye Yint <mryeyintaung669@gmail.com>
2025-08-15 21:20:18 +0630

    temp fixed in lazyvim

--- .config/nvim/lua/plugins/color.lua
+++ .config/nvim/lua/plugins/color.lua
@@ -2,20 +2,21 @@ return {
 
     {
         "olimorris/onedarkpro.nvim",
-        priority = 1000, 
-    config = function()
-        require("onedarkpro").setup({
-  options = {
-    transparency = true
-  }
-})
-    end
+        priority = 1000,
+        config = function()
+            require("onedarkpro").setup({
+                options = {
+                    transparency = true,
+                },
+            })
+        end,
     },
+    { "ellisonleao/gruvbox.nvim", priority = 1000, config = true, opts = ... },
 
     {
         "LazyVim/LazyVim",
         opts = {
-            colorscheme = "vaporwave",
+            colorscheme = "gruvbox",
         },
     },
 }
