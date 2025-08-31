7a81801eca46f57519382762afbbdee7453dfa06
Ye Yint <mryeyintaung669@gmail.com>
2025-08-31 23:05:42 +0630

    solarized

--- .config/nvim/lua/plugins/color.lua
+++ .config/nvim/lua/plugins/color.lua
@@ -1,11 +1,32 @@
 return {
 
-    { "EdenEast/nightfox.nvim" },
+    {
+        "sainnhe/sonokai",
+        lazy = false,
+        priority = 1000,
+        config = function()
+            -- Optionally configure and load the colorscheme
+            -- directly inside the plugin declaration.
+        end,
+    },
+    {
+        "maxmx03/solarized.nvim",
+        lazy = false,
+        priority = 1000,
+        ---@type solarized.config
+        opts = {
+            variant = "autumn", -- "spring" | "summer" | "autumn" | "winter" (default)
+            transparent = { enabled = false },
+        },
+        config = function(_, opts)
+            require("solarized").setup(opts)
+        end,
+    },
 
     {
         "LazyVim/LazyVim",
         opts = {
-            colorscheme = "nightfox",
+            colorscheme = "solarized",
         },
     },
 }
