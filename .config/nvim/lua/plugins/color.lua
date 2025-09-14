bf8def4708395ffe333d63762b00967703af1186
Ye Yint <mryeyintaung669@gmail.com>
2025-09-14 14:14:00 +0630

    idea vim rc

--- .config/nvim/lua/plugins/color.lua
+++ .config/nvim/lua/plugins/color.lua
@@ -1,15 +1,6 @@
 return {
 
     {
-        "sainnhe/sonokai",
-        lazy = false,
-        priority = 1000,
-        config = function()
-            -- Optionally configure and load the colorscheme
-            -- directly inside the plugin declaration.
-        end,
-    },
-    {
         "maxmx03/solarized.nvim",
         lazy = false,
         priority = 1000,
