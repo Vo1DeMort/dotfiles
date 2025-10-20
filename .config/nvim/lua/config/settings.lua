50c52982a47f9df75dac458ef411d9d75ce0aaa5
Ye Yint <mryeyintaung669@gmail.com>
2025-10-20 22:49:40 +0630

    some changes

--- .config/nvim/lua/config/settings.lua
+++ .config/nvim/lua/config/settings.lua
@@ -1,11 +1,15 @@
 -- disable mouse
 vim.opt.mouse =
+    -- enable lsp lines
+    vim.diagnostic.config({
+        virtual_text = false, -- Disable normal error/warning lines
+        virtual_lines = true, -- Enable virtual lines
+        signs = true, -- Show signs in the sign column
+        underline = true, -- Underline the problematic code
+        update_in_insert = false, -- Don't update diagnostics in insert mode
+    })
 
--- enable lsp lines
-vim.diagnostic.config({
-    virtual_text = false, -- Disable normal error/warning lines
-    virtual_lines = true, -- Enable virtual lines
-    signs = true, -- Show signs in the sign column
-    underline = true, -- Underline the problematic code
-    update_in_insert = false, -- Don't update diagnostics in insert mode
-})
+vim.opt.foldmethod = "expr"
+vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
+vim.opt.foldlevel = 99 -- open everything except what you explicitly close
+vim.opt.foldenable = true
