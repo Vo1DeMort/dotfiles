b228dad504ae5c257028cd555a4dfff50227dc7e
Ye Yint <mryeyintaung669@gmail.com>
2025-10-27 19:44:58 +0630

    add atac in nvim

---
+++ .config/nvim/lua/plugins/atac.lua
@@ -0,0 +1,12 @@
+-- this an api clinet just like postman but in terminal
+return {
+    {
+        "NachoNievaG/atac.nvim",
+        dependencies = { "akinsho/toggleterm.nvim" },
+        config = function()
+            require("atac").setup({
+                dir = "~/my/work/directory", -- By default, the dir will be set as /tmp/atac
+            })
+        end,
+    },
+}
